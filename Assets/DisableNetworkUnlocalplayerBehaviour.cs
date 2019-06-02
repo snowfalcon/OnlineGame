bb266c88a4b13e18317d5533d9616fec084e94c0
snowfalcon <snowfalcon1206@yahoo.co.jp>
2019-06-02 23:51:24 +0900

    .gitignoreと.gitattributesの追加

---
+++ Assets/DisableNetworkUnlocalplayerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableNetworkUnlocalplayerBehaviour : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] behaviours;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (var behaviour in behaviours)
+            {
+                behaviour.enabled = false;
+            }
+        }
+    }
+
+    void OnApplicationFocus(bool focusStatus)
+    {
+        if (isLocalPlayer)
+        {
+            foreach (var behaviour in behaviours)
+            {
+                behaviour.enabled = focusStatus;
+            }
+        }
+    }
+}
